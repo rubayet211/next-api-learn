cccf713ffab6910013f77892c6a0c4a865b323d9
Rhyme Rubayet <rubayet211@gmail.com>
2024-07-08 06:18:10 +0600

    Middleware added for authentication for blogs

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import { authMiddleware } from "./middlewares/api/authMiddleware";
+
+export const config = {
+  matcher: "/api/:path*",
+};
+
+export default function middleware(request: Request) {
+  const authResult = authMiddleware(request);
+  if (!authResult.isValid || request.url.includes("/api/blogs")) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+  return NextResponse.next();
+}
